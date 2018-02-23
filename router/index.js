1f4817fba779ff6d56454116aa8f073fe8fa738d
Marcos Moritz <marcosmoritz@gmail.com>
2018-02-23 05:02:04 -0300

    using the Router

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ const express = require( "express" );
 const http = require( "http" );
 const bodyParser = require( "body-parser" );
 const morgan = require( "morgan" );
+const dishRouter = require( "./routes/dishRouter" );
 
 const host = "localhost";
 const port = 3000;
@@ -9,51 +10,7 @@ const port = 3000;
 const app = express();
 app.use( bodyParser.json() );
 app.use( morgan( "dev" ) );
-
-app.all( "/dishes", ( req, res, next ) => {
-  res.statusCode = 200;
-  res.setHeader( "Content-Type", "text/plain" );
-  next();
-} );
-
-app.get( "/dishes", ( req, res, next ) => {
-  res.end( "Will send all the dishes to you" );
-} );
-
-app.post( "/dishes", ( req, res, next ) => {
-  const { name, description } = req.body;
-  res.end( `Will add the dish ${name} with details ${description}` );
-} );
-
-app.put( "/dishes", ( req, res, next ) => {
-  res.statusCode = 403;
-  res.end( "PUT operation not supported /dishes" );
-} );
-
-app.delete( "/dishes", ( req, res, next ) => {
-  res.end( "Deleting all the dishes!" );
-} );
-
-app.get( "/dishes/:dishId", ( req, res, next ) => {
-  res.end( `Will send details of ${req.params.dishId}` );
-} );
-
-app.post( "/dishes/:dishId", ( req, res, next ) => {
-  const { name, description } = req.body;
-  res.end( `POST operation is not supperted in /dishes/:dishId` );
-} );
-
-app.put( "/dishes/:dishId", ( req, res, next ) => {
-  const { name, description } = req.body;
-
-  res.statusCode = 403;
-  res.write( `Update the dish: ${req.params.dishId}` );
-  res.end( `Will update the dish: ${name} with details: ${descrition}` );
-} );
-
-app.delete( "/dishes/:dishId", ( req, res, next ) => {
-  res.end( `Deleting the dish ${req.params.dishId}` );
-} );
+app.use( "/dishes", dishRouter );
 
 app.use( express.static( `${__dirname}/public` ) );
 
---
+++ router/routes/dishRouter.js
@@ -0,0 +1,28 @@
+const express = require( "express" );
+const bodyParser = require( "body-parser" );
+
+const dishRouter = express.Router();
+dishRouter.use( bodyParser.json() );
+
+dishRouter.route( "/" )
+  .all( ( req, res, next ) => {
+    res.statusCode = 200;
+    res.setHeader( "Content-Type", "text/plain" );
+    next();
+  } )
+  .get( ( req, res, next ) => {
+    res.end( "Will send all the dishes to you" );
+  } )
+  .post( ( req, res, next ) => {
+    const { name, description } = req.body;
+    res.end( `Will add the dish ${name} with details ${description}` );
+  } )
+  .put( ( req, res, next ) => {
+    res.statusCode = 403;
+    res.end( "PUT operation not supported /dishes" );
+  } )
+  .delete( ( req, res, next ) => {
+    res.end( "Deleting all the dishes!" );
+  } );
+
+module.exports = dishRouter;
