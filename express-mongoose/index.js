bae5b2a0842a690e26f3e8f69d641fa216c67744
Marcos Moritz <marcosmoritz@gmail.com>
2018-03-06 05:58:34 -0300

    Rename projects

--- express-mongoose/index.js
+++ express-mongoose/index.js
@@ -1,14 +1,32 @@
+const path = require('path');
+
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const Dishes = require('./models/dishes');
+
 const url = 'mongodb://localhost:27017/conFusion';
+const host = 'http://localhost';
+const port = 3000;
 
 const connect = mongoose.connect(url, {});
 connect.then((conn) => {
 
 });
+
 var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// static files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('dishes', dishRouter);
 
 app.listen(3000);
---
+++ express-router/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const dishRouter = require('./routes/dishRouter');
+
+const host = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use('/dishes', dishRouter);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>Hello</h1></body></html>');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, host, () => {
+  console.log(`Running at http://${host}:${port}`);
+});
