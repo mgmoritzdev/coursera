71bb7b8326022e2e19342eb8eee5282b633aea80
Marcos Moritz <marcosmoritz@gmail.com>
2018-03-06 09:23:43 -0300

    First part of express-mongoose
    - the part that matters
    - the second part is for the routes for comments, all the same applies

--- express-mongoose/index.js
+++ express-mongoose/index.js
@@ -7,10 +7,12 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const host = 'http://localhost';
 const port = 3000;
 
 const connect = mongoose.connect(url, {});
@@ -27,6 +29,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('dishes', dishRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
-app.listen(3000);
+app.listen(port);
--- express-mongoose/routes/dishRouter.js
+++ express-mongoose/routes/dishRouter.js
@@ -1,54 +1,77 @@
-const express = require( "express" );
-const bodyParser = require( "body-parser" );
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
+
+const Dishes = require('../models/dishes');
 
 const dishRouter = express.Router();
-dishRouter.use( bodyParser.json() );
+dishRouter.use(bodyParser.json());
 
-dishRouter.route( "/" )
-  .all( ( req, res, next ) => {
+dishRouter.route('/')
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader( "Content-Type", "text/plain" );
+    res.setHeader('Content-Type', 'application/json');
     next();
-  } )
-  .get( ( req, res, next ) => {
-    res.end( "Will send all the dishes to you" );
-  } )
-  .post( ( req, res, next ) => {
-    const { name, description } = req.body;
-    res.end( `Will add the dish ${name} with details ${description}` );
-  } )
-  .put( ( req, res, next ) => {
+  })
+  .get((req, res, next) => {
+    Dishes.find({})
+      .then((dishes) => {
+        res.json(dishes);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    Dishes.create(req.body)
+      .then((dish) => {
+        res.json(dish);
+      });
+  })
+  .put((req, res, next) => {
     res.statusCode = 403;
-    res.end( "PUT operation not supported /dishes" );
-  } )
-  .delete( ( req, res, next ) => {
-    res.end( "Deleting all the dishes!" );
-  } );
+    res.end('PUT operation not supported /dishes');
+  })
+  .delete((req, res, next) => {
+    Dishes.remove({})
+      .then((response) => {
+        res.json(response);
+      });
+  });
 
-dishRouter.route( "/:dishId" )
-  .all( ( req, res, next ) => {
+dishRouter.route('/:dishId')
+  .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader( "Content-Type", "text/plain" );
+    res.setHeader('Content-Type', 'application/json');
     next();
-  } )
-  .get( ( req, res, next ) => {
-    res.end( `Will send to you the dish ${req.params.dishId}` );
-  } )
-  .post( ( req, res, next ) => {
+  })
+  .get((req, res, next) => {
+    Dishes
+      .findById(req.params.dishId)
+      .then((dish) => {
+        res.json(dish);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
     res.statusCode = 403;
-    res.end( "POST operation not supported /dishes" );
-  } )
-  .put( ( req, res, next ) => {
-    res.statusCode = 403;
-    const response = [
-      `Will modify the dish ${req.params.dishId}`,
-      `with name = ${req.body.name} and`,
-      `description = ${req.body.description}`
-    ].join( " " );
-    res.end( response );
-  } )
-  .delete( ( req, res, next ) => {
-    res.end( `Will delete the dish with id = ${req.params.dishId}` );
-  } );
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('POST operation not supported /dishes');
+  })
+  .put((req, res, next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+      $set: req.body
+    }, {new: true})
+      .then((dish) => {
+        res.json(dish);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  })
+  .delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+      .then((response) => {
+        res.json(response);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  });
 
 module.exports = dishRouter;
